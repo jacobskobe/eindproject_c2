15c5f08a0f78f4884f08ebcff231583506482129
KobeJ-2261712 <kobe.jacobs@student.uhasselt.be>
2024-05-06 21:37:41 +0200

    Made function to turn cmd argument into path name to check for file

--- main_dnd.c
+++ main_dnd.c
@@ -18,24 +18,40 @@ struct Item {
 
 };
 
+
+void path_maker(char *path, char *file_name);
+
 int main(int argc, char* argv[]){
+    char *path_ptr;
+    char path_str[50] = "../json_files/";
+    path_ptr = path_str;
+    
+
 
     // Put all given file data into structs to add to linked list
-    for (int i = 0; i < argc, i++){
+    for (int i = 1; i < argc; i++){        
+        
+    
+        path_maker(path_ptr, argv[i]);
+        
         // check if argument is a json file
+        // Make path from file name
         // pass to parse function if argument is json file
 
         // else skip
-        
+        printf("%s\n", path_ptr);
+        strcpy(path_ptr, "../json_files/");
     }
 
 
+    
+
+    free(path_ptr);
 
 }
 
-
-// Parse given file into the struct
-void item_to_struct_parser(struct Item * item_to_fill, char* file_name) {
-
+void path_maker(char *path, char *file_name){
+    strcat(path, file_name);
 }
+
 
