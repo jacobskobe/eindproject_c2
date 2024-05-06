445feb518dd0010fdef0e9630bb94a0c6c1b3ff2
KobeJ-2261712 <kobe.jacobs@student.uhasselt.be>
2024-05-06 20:14:48 +0200

    Added basic struct layout, function blueprint. Also added json file folder.

--- main_dnd.c
+++ main_dnd.c
@@ -1,14 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 
+// dnd object struct
+struct Item {
 
+    char name;
+    int weight;
+    struct item_cost {
+        int cost;
+        char cost_unit[2];
+    };
+    char weapon_category;
+    char description;
+
+};
 
 int main(int argc, char* argv[]){
+
+    // Put all given file data into structs to add to linked list
+    for (int i = 0; i < argc, i++){
+        // check if argument is a json file
+        // pass to parse function if argument is json file
+
+        // else skip
+        
+    }
 
 
 
 }
 
 
+// Parse given file into the struct
+void item_to_struct_parser(struct Item * item_to_fill, char* file_name) {
 
+}
 
-// parse all given files into a linked list containing the structs
