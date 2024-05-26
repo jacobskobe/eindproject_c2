df2d03db58bc139ba6dd990312f8777a932aac07
KobeJ-2261712 <kobe.jacobs@student.uhasselt.be>
2024-05-26 14:36:07 +0200

    Make function to traverse linked list item by item, user can decide when to quit

--- main_dnd.c
+++ main_dnd.c
@@ -30,6 +30,7 @@ void fill_item_from_json(struct Item *item, const char *filename);
 char *read_file(const char *filename);
 cJSON *parse_json(const char *json_data);
 void extract_item(const cJSON *json, struct Item *item);
+void traverse_list(struct item_list *head);
 
 int main(int argc, char* argv[]) {
 
@@ -60,7 +61,7 @@ int main(int argc, char* argv[]) {
                     // create first node for linked list
                     head = (struct item_list *)malloc(sizeof(struct item_list));
                     head->item_dnd = new_item;
-                    head->next_item = NULL;
+                    head->next_item = head;  // Circular link
                     counter_list++;
                 } else {
                     // Make new object to add to the linked list (push)
@@ -77,20 +78,21 @@ int main(int argc, char* argv[]) {
         strcpy(path_ptr, "C:\\Users\\kobej\\Desktop\\skorro23_24\\C2\\Eindproject_KobeJacobs\\json_files\\");
     }
 
-    // Print and free the linked list
-    struct item_list *current = head;
-    while (current != NULL) {
-        printf("Name: %s\n", current->item_dnd->name);
-        printf("Weight: %.2f\n", current->item_dnd->weight);  // Updated to print double
-        printf("Cost: %d %s\n", current->item_dnd->cost.cost, current->item_dnd->cost.cost_unit);
-        printf("Description: %s\n\n", current->item_dnd->description);
-        struct item_list *temp = current;
-        current = current->next_item;
-        free(temp->item_dnd->name);
-        free(temp->item_dnd->cost.cost_unit);
-        free(temp->item_dnd->description);
-        free(temp->item_dnd);
-        free(temp);
+    // Traverse the list
+    traverse_list(head);
+
+    // Print and free the circular linked list
+    if (head != NULL) {
+        struct item_list *current = head;
+        do {
+            struct item_list *temp = current;
+            current = current->next_item;
+            free(temp->item_dnd->name);
+            free(temp->item_dnd->cost.cost_unit);
+            free(temp->item_dnd->description);
+            free(temp->item_dnd);
+            free(temp);
+        } while (current != head);
     }
 
     return 0;
@@ -117,13 +119,14 @@ int check_file_exist(const char *file_path) {
 void push_to_list(struct item_list **head, struct Item *object_to_push) {
     struct item_list *new_item = (struct item_list *)malloc(sizeof(struct item_list));
     new_item->item_dnd = object_to_push;
-    new_item->next_item = NULL;
+    new_item->next_item = *head;
 
     if (*head == NULL) {
         *head = new_item;
+        new_item->next_item = new_item;  // Circular link
     } else {
         struct item_list *current = *head;
-        while (current->next_item != NULL) {
+        while (current->next_item != *head) {
             current = current->next_item;
         }
         current->next_item = new_item;
@@ -244,4 +247,34 @@ void fill_item_from_json(struct Item *item, const char *filename) {
 
     cJSON_Delete(json);
     free(json_data);
+}
+
+// Function to traverse the circular linked list
+void traverse_list(struct item_list *head) {
+    if (head == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+
+    struct item_list *current = head;
+    char input;
+
+    do {
+        printf("Name: %s\n", current->item_dnd->name);
+        printf("Weight: %.2f\n", current->item_dnd->weight);
+        printf("Cost: %d %s\n", current->item_dnd->cost.cost, current->item_dnd->cost.cost_unit);
+        printf("Description: %s\n\n", current->item_dnd->description);
+
+        printf("Press 'n' to go to the next item, or any other key to exit: ");
+        input = getchar();
+        getchar(); // Consume the newline character
+        printf("\n");
+
+        if (input == 'n') {
+            current = current->next_item;
+        } else {
+            break;
+        }
+
+    } while (current != head || input == 'n'); // Loop until we're back at the start and the user wants to exit
 }
